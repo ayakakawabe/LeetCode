33dc208922cf96586c9f7bc3792d9511c89daa59
ayakakawabe <kawabe.ayaka@icloud.com>
2025-02-24 17:10:00 +0900

    2025-02-24 17:09:59

---
+++ src/tree/python3/100_SameTree.py
@@ -0,0 +1,34 @@
+#
+# @lc app=leetcode id=100 lang=python3
+#
+# [100] Same Tree
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        def preorder_compare(root1,root2,is_same):
+            if is_same:
+                if root1 and root2:
+                    if root1.val != root2.val:
+                        is_same=False
+                        print(False)
+                    else:
+                        is_same=preorder_compare(root1.left,root2.left,is_same)
+                        is_same=preorder_compare(root1.right,root2.right,is_same)
+                elif (root1==None) and (root2==None):
+                    pass
+                else:
+                    is_same=False
+            return is_same
+            
+        return preorder_compare(p,q,True)
+
+# @lc code=end
+
