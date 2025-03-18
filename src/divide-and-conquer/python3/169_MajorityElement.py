97faec8b9bc2c004151a48ec5a6e4fb9556a0f82
ayakakawabe <kawabe.ayaka@icloud.com>
2025-03-18 13:15:46 +0900

    2025-03-18 13:15:45

--- src/divide-and-conquer/python3/169_MajorityElement.py
+++ src/divide-and-conquer/python3/169_MajorityElement.py
@@ -7,6 +7,22 @@
 # @lc code=start
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
+        # def quick_sort(array: List[int]) -> List[int]:
+        #     if len(array) <= 1:
+        #         return array
+        #     piv=array.pop(0)
+        #     left=[]
+        #     right=[]
+        #     for i in range(0,len(array),1):
+        #         if array[i] <= piv:
+        #             left.append(array[i])
+        #         else:
+        #             right.append(array[i])
+        #     left=quick_sort(left)
+        #     right=quick_sort(right)
+        #     return left+[piv]+right
+        
+        # nums=quick_sort(nums)
         nums.sort()
         return nums[int(len(nums)/2)]
 # @lc code=end
