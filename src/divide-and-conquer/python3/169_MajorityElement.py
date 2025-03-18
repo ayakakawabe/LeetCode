59cf9f562e764fc27f9cd47401c80d39b24df53e
ayakakawabe <kawabe.ayaka@icloud.com>
2025-03-18 13:04:12 +0900

    2025-03-18 13:04:10

--- src/divide-and-conquer/python3/169_MajorityElement.py
+++ src/divide-and-conquer/python3/169_MajorityElement.py
@@ -8,14 +8,6 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         nums.sort()
-        if len(nums)%2 == 0:
-            for i in range(0,int(len(nums)/2),1):
-                if nums[i] == nums[i+int(len(nums)/2)]:
-                    return nums[i]
-        else:
-            for i in range(0,int(len(nums)/2)+1,1):
-                if nums[i] == nums[i+int(len(nums)/2)]:
-                    return nums[i]
-
+        return nums[int(len(nums)/2)]
 # @lc code=end
 
